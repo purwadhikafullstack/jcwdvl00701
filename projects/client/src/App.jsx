99c978ad147467620793706a1d3babe9961c2572
Muhammad Rofi <98885565+Rofi11@users.noreply.github.com>
2022-11-16 17:41:34 +0700

    test

--- projects/client/src/App.jsx
+++ projects/client/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Test from "./Components/Test";
+import LoginUser from "./Page/loginUser";
 
 class App extends React.Component {
   render() {
     return (
         <BrowserRouter>
           <Switch>
+            <Route component={LoginUser} path="/loginUser" />
             <Route component={Test} path="/" />
           </Switch>
         </BrowserRouter>
