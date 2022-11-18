6bb7f9a5e2a4ff6f213778fbd798fb7e4e43568a
imamahmad <90876461+imamahmadf@users.noreply.github.com>
2022-11-18 23:22:12 +0700

    feat: Booking history

--- projects/client/src/Page/User/bookingHistory.js
+++ projects/client/src/Page/User/bookingHistory.js
@@ -8,7 +8,7 @@ import {
   Center,
 } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
-import Gambar from "../../Assets/gambar1.png";
+import Image1 from "../../Assets/bookingHistory1.png";
 import Image2 from "../../Assets/bookingHistory2.png";
 import Image3 from "../../Assets/bookingHistory3.png";
 import Footer from "../../Components/Footer";
@@ -17,6 +17,16 @@ import Footer from "../../Components/Footer";
 function BookingHistory() {
   const booking = [
     {
+      pic: Image1,
+      name: "Apartment in Jakarta",
+      guest_count: 1,
+      start_date: 18,
+      end_date: 19,
+      price: 800000,
+      satatus: "ongoing",
+      roomName: "room 2",
+    },
+    {
       pic: Image2,
       name: "Apartment in Bandung",
       guest_count: 1,
@@ -24,6 +34,7 @@ function BookingHistory() {
       end_date: 14,
       price: 600000,
       satatus: "finished",
+      roomName: "room 2",
     },
     {
       pic: Image3,
@@ -33,6 +44,7 @@ function BookingHistory() {
       end_date: 15,
       price: 700000,
       satatus: "cancled",
+      roomName: "room 2",
     },
   ];
   const [bookingHis, setBookingHis] = useState(booking);
@@ -142,9 +154,91 @@ function BookingHistory() {
     });
   }
 
+  function renderBooking() {
+    return booking.map((val) => {
+      if (val.satatus === "ongoing") {
+        return (
+          <Box bg="primary" w="100%" p="20px" color="white">
+            <Box
+              backgroundImage={val.pic}
+              backgroundPosition="cover"
+              backgroundRepeat="no-repeat"
+              height="130px"
+            >
+              <Box bg="rgba(45, 53, 249, 1)" w="110px" h="30px" color="white">
+                <Text py="4px" px="25px" fontSize="15px">
+                  {val.satatus}
+                </Text>
+              </Box>
+            </Box>
+            <Flex bg="white" color="black" mt="20px" p="10px">
+              <Box me="10px">
+                <i className="fa-solid fa-bed"></i>
+              </Box>
+              <Box>
+                <Text fontWeight="bold" fontSize="18px">
+                  {val.name}
+                </Text>
+              </Box>
+            </Flex>
+            <Flex bg="white" color="black" p="10px" pt="0px">
+              <Text fontWeight="regular" fontSize="14px" w="110px">
+                Chek-in
+              </Text>
+              <Text fontWeight="regular" fontSize="14px" w="130px">
+                Sun, {val.start_date} Nov 2022 (14:00-22:00)
+              </Text>
+            </Flex>
+            <Flex
+              bg="white"
+              color="black"
+              p="10px"
+              pt="0px"
+              borderBottom="1px"
+              borderColor="gray.200"
+            >
+              <Text fontWeight="regular" fontSize="14px" w="110px">
+                Chek-out
+              </Text>
+              <Text fontWeight="regular" fontSize="14px" w="130px">
+                Sun, {val.end_date} Nov 2022 (00:00-12:00)
+              </Text>
+            </Flex>
+            <Text
+              fontWeight="regular"
+              fontSize="14px"
+              bg="white"
+              color="black"
+              px="10px"
+              pt="10px"
+            >
+              (1x) {val.roomName}
+            </Text>
+            <Text
+              fontWeight="regular"
+              fontSize="12px"
+              bg="white"
+              color="rgba(175, 175, 175, 1)"
+              px="10px"
+              pb="20px"
+            >
+              {val.guest_count} Guests
+            </Text>
+          </Box>
+        );
+      }
+    });
+  }
+
   return (
-    <Box bg="tomato" w="100%" h="90px">
-      <Box bg="white" w="100%" p="20px">
+    <Box w="100%" h="90px">
+      <Box
+        bg="white"
+        w="100%"
+        p="20px"
+        borderBottom="1px"
+        borderColor="gray.200"
+      >
         <Flex>
           <Box boxSize="50px">
             <Image src={"https://bit.ly/dan-abramov"} alt="Dan Abramov" />
@@ -164,73 +258,7 @@ function BookingHistory() {
         </Flex>
       </Box>
 
-      <Box bg="primary" w="100%" p="20px" color="white">
-        <Box
-          backgroundImage={Gambar}
-          backgroundPosition="cover"
-          backgroundRepeat="no-repeat"
-          height="130px"
-        >
-          <Box bg="rgba(45, 53, 249, 1)" w="110px" h="30px" color="white">
-            <Text py="4px" px="25px" fontSize="15px">
-              Ongoing
-            </Text>
-          </Box>
-        </Box>
-        <Flex bg="white" color="black" mt="20px" p="10px">
-          <Box me="10px">
-            <i className="fa-solid fa-bed"></i>
-          </Box>
-          <Box>
-            <Text fontWeight="bold" fontSize="18px">
-              Apartment in Jakarta
-            </Text>
-          </Box>
-        </Flex>
-        <Flex bg="white" color="black" p="10px" pt="0px">
-          <Text fontWeight="regular" fontSize="14px" w="110px">
-            Chek-in
-          </Text>
-          <Text fontWeight="regular" fontSize="14px" w="130px">
-            Sun, 12 Nov 2022 (14:00-22:00)
-          </Text>
-        </Flex>
-        <Flex
-          bg="white"
-          color="black"
-          p="10px"
-          pt="0px"
-          borderBottom="1px"
-          borderColor="gray.200"
-        >
-          <Text fontWeight="regular" fontSize="14px" w="110px">
-            Chek-out
-          </Text>
-          <Text fontWeight="regular" fontSize="14px" w="130px">
-            Sun, 16 Nov 2022 (14:00-22:00)
-          </Text>
-        </Flex>
-        <Text
-          fontWeight="regular"
-          fontSize="14px"
-          bg="white"
-          color="black"
-          px="10px"
-          pt="10px"
-        >
-          (1x) Room 1
-        </Text>
-        <Text
-          fontWeight="regular"
-          fontSize="12px"
-          bg="white"
-          color="rgba(175, 175, 175, 1)"
-          px="10px"
-          pb="20px"
-        >
-          2 Guests
-        </Text>
-      </Box>
+      {renderBooking()}
       <Box bg="white" w="100%" py="30px" px="20px">
         <Text fontWeight="Bold" fontSize="22px" pb="20px">
           History Booking:
