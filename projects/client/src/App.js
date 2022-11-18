599178671a3263fbbb5ae50d43b94a35c21c5cea
imamahmad <90876461+imamahmadf@users.noreply.github.com>
2022-11-18 19:13:20 +0700

    feat: history_booking
    page history booking

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -5,7 +5,7 @@ import Test from "./Components/Test";
 import LoginUser from "./Page/User/loginUser";
 import RegisterUser from "./Page/User/registerUser";
 import LoginTenant from "./Page/Tenant/loginTenant";
-import RegisterTenant from "./Page/Tenant/registerTenant"
+import RegisterTenant from "./Page/Tenant/registerTenant";
 import BookingHistory from "./Page/User/bookingHistory";
 import Booking from "./Page/User/booking";
 import Payment from "./Page/User/payment";
@@ -18,23 +18,26 @@ import VerifyForgot from "./Page/verifyForgot";
 class App extends React.Component {
   render() {
     return (
-        <BrowserRouter>
-          <Switch>
-            <Route component={VerifyForgot} path="/account/password/forgot/verify" />
-            <Route component={VerifyAccount} path="/account/verify" />
-            <Route component={ChangePassword} path="/account/password/change" />
-            <Route component={ResetPassword} path="/account/password/reset" />
-            <Route component={ForgotPassword} path="/account/password/forgot" />
-            <Route component={RegisterTenant} path="/tenant/register" />
-            <Route component={LoginTenant} path="/tenant/login" />
-            <Route component={RegisterUser} path="/register" />
-            <Route component={LoginUser} path="/login" />
-            <Route component={BookingHistory} path="/booking-history" />
-            <Route component={Booking} path="/booking" />
-            <Route component={Payment} path="/payment" />
-            <Route component={Test} path="/" />
-          </Switch>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Switch>
+          <Route
+            component={VerifyForgot}
+            path="/account/password/forgot/verify"
+          />
+          <Route component={VerifyAccount} path="/account/verify" />
+          <Route component={ChangePassword} path="/account/password/change" />
+          <Route component={ResetPassword} path="/account/password/reset" />
+          <Route component={ForgotPassword} path="/account/password/forgot" />
+          <Route component={RegisterTenant} path="/tenant/register" />
+          <Route component={LoginTenant} path="/tenant/login" />
+          <Route component={RegisterUser} path="/register" />
+          <Route component={LoginUser} path="/login" />
+          <Route component={BookingHistory} path="/booking-history" />
+          <Route component={Booking} path="/booking" />
+          <Route component={Payment} path="/payment" />
+          <Route component={Test} path="/" />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
