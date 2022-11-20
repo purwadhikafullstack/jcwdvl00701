8fe5f2baf8fea708f747d1b97193efa75e3bb2e2
Muhammad Rofi <98885565+Rofi11@users.noreply.github.com>
2022-11-20 20:19:06 +0700

    sudah beres password page

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -6,12 +6,22 @@ import LoginUser from "./Page/User/loginUser";
 import RegisterUser from "./Page/User/registerUser";
 import LoginTenant from "./Page/Tenant/loginTenant";
 import RegisterTenant from "./Page/Tenant/registerTenant"
+import ForgotPassword from "./Page/forgotPassword";
+import ResetPassword from "./Page/resetPassword";
+import ChangePassword from "./Page/changePassword";
+import VerifyAccount from "./Page/verifyAccount";
+import VerifyForgot from "./Page/verifyForgot";
 
 class App extends React.Component {
   render() {
     return (
         <BrowserRouter>
           <Switch>
+            <Route component={VerifyForgot} path="/account/password/forgot/verify" />
+            <Route component={VerifyAccount} path="/account/verify" />
+            <Route component={ChangePassword} path="/account/password/change" />
+            <Route component={ResetPassword} path="/account/password/reset" />
+            <Route component={ForgotPassword} path="/account/password/forgot" />
             <Route component={RegisterTenant} path="/tenant/register" />
             <Route component={LoginTenant} path="/tenant/login" />
             <Route component={RegisterUser} path="/register" />
