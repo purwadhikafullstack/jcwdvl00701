9ed6ade6a2e233f749bff03a9d173a47266e512c
Muhammad Rofi <98885565+Rofi11@users.noreply.github.com>
2022-11-21 09:00:58 +0700

    Feat password page (#9)
    * test
    
    * selesai UI login/register
    
    * Update Footer.js
    
    * sudah beres password page

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -5,23 +5,30 @@ import Test from "./Components/Test";
 import LoginUser from "./Page/User/loginUser";
 import RegisterUser from "./Page/User/registerUser";
 import LoginTenant from "./Page/Tenant/loginTenant";
-import RegisterTenant from "./Page/Tenant/registerTenant";
-import BookingHistory from "./Page/User/bookingHistory";
+import RegisterTenant from "./Page/Tenant/registerTenant"
+import ForgotPassword from "./Page/forgotPassword";
+import ResetPassword from "./Page/resetPassword";
+import ChangePassword from "./Page/changePassword";
+import VerifyAccount from "./Page/verifyAccount";
+import VerifyForgot from "./Page/verifyForgot";
 
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route component={RegisterTenant} path="/tenant/register" />
-          <Route component={LoginTenant} path="/tenant/login" />
-          <Route component={RegisterUser} path="/register" />
-          <Route component={LoginUser} path="/login" />
-
-          <Route component={BookingHistory} path="/booking-history" />
-          <Route component={Test} path="/" />
-        </Switch>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Switch>
+            <Route component={VerifyForgot} path="/account/password/forgot/verify" />
+            <Route component={VerifyAccount} path="/account/verify" />
+            <Route component={ChangePassword} path="/account/password/change" />
+            <Route component={ResetPassword} path="/account/password/reset" />
+            <Route component={ForgotPassword} path="/account/password/forgot" />
+            <Route component={RegisterTenant} path="/tenant/register" />
+            <Route component={LoginTenant} path="/tenant/login" />
+            <Route component={RegisterUser} path="/register" />
+            <Route component={LoginUser} path="/login" />
+            <Route component={Test} path="/" />
+          </Switch>
+        </BrowserRouter>
     );
   }
 }
