eec9e66164393b0923c679527d61b3e59b797b76
Muhammad Rofi <98885565+Rofi11@users.noreply.github.com>
2022-12-15 23:18:16 +0700

    Feat register/tenant (#39)
    * test
    
    * selesai UI login/register
    
    * Update Footer.js
    
    * ganti tempat dependencies font dan benerin error
    
    * menghilangkan font client dari package.json lerna
    
    * register & complete tenant sudah beres

--- projects/client/src/Components/Layout.js
+++ projects/client/src/Components/Layout.js
@@ -1,5 +1,6 @@
 import Footer from "./Footer";
 import NavbarTuru from "./NavbarTuru";
+import NavbarDestop from "./NavbarDestop";
 
 function Layout({ children }) {
   return (
--- projects/server/src/routes/index.js
+++ projects/server/src/routes/index.js
@@ -1,9 +1,11 @@
 const userRouters = require("./userRouters")
 const roomRouters = require("./roomRouters")
 const propertyRouters = require("./propertyRouters");
+const tenantRouters = require("./tenantRouters")
 
 module.exports = {
     userRouters,
     roomRouters,
     propertyRouters,
+    tenantRouters
 }
