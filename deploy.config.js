ad7677bf9884c67dc4a00bbdb67d5eb404a2ade0
Kuntiarso <60772041+kuntiarso@users.noreply.github.com>
2022-12-22 15:46:31 +0700

    Revert "Release v2"

--- deploy.config.js
+++ deploy.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: "JCWdVL-007-01", // Format JCWD-{batchcode}-{groupnumber}
+      name: "JCWD-2000-01", // Format JCWD-{batchcode}-{groupnumber}
       script: "./projects/server/src/index.js",
       env: {
         NODE_ENV: "production",
-        PORT: 8701,
+        PORT: 8000,
       },
       time: true,
     },
