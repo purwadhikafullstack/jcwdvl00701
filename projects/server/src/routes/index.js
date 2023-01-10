25611a6c1afc93cffa33c34e100267288506f495
makse <maxx23295@gmail.com>
2023-01-10 20:41:22 +0700

    room unavailability fixed

--- projects/server/src/routes/index.js
+++ projects/server/src/routes/index.js
@@ -3,11 +3,13 @@ const specialPriceRouters = require("./specialPriceRouters")
 const roomRouters = require("./roomRouters")
 const propertyRouters = require("./propertyRouters");
 const tenantRouters = require("./tenantRouters")
+const roomUnavailabilityRouters = require("./roomUnavailabilityRouters")
 
 module.exports = {
     userRouters,
     roomRouters,
     tenantRouters,
     specialPriceRouters,
-    propertyRouters
+    propertyRouters,
+    roomUnavailabilityRouters
 }
