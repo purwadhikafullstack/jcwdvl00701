174c2c708dff66d4883ab8826fe2e680ea3c8dfe
imamahmad <90876461+imamahmadf@users.noreply.github.com>
2023-01-11 23:30:34 +0700

    bug-fixing
    merge dengan develop

--- projects/client/src/Pages/Tenant/completeFormTenant.js
+++ projects/client/src/Pages/Tenant/completeFormTenant.js
@@ -85,7 +85,7 @@ function CompleteFormTenant() {
         .then((res) => {
           console.log(res.data);
           alert(res.data.message);
-          history.push("/");
+          history.push("/tenant/dashboard");
         })
         .catch((err) => {
           console.error(err.data.message);
@@ -121,36 +121,20 @@ function CompleteFormTenant() {
               </Flex>
             </Box>
 
-            await axios.post(`${process.env.REACT_APP_API_BASE_URL}/tenant/complete-register`, formData)
-            .then((res) => {
-                console.log(res.data);
-                alert(res.data.message)
-                history.push("/tenant/dashboard")
-            })
-            .catch((err) => {
-                console.error(err.data.message)
-            })
-        }
-    })
-    return (
-        <>
-        <Container maxW="2x1" px="0px">
-            <Flex flexDirection="column" bg="black">
-            {/* flex container utk dekstop */}
-            <Flex>
-                {/* utk image dekstop */}
-                <Box
-                width="900px"
-                display={{ ss: "none", sm: "none", md: "block" }}
-                >
-                <Flex>
-                    <Image
-                    display={{
-                        ss: "none",
-                        sm: "none",
-                        sl: "none",
-                        md: "block",
-                        lg: "block",
+            {/* Form */}
+            <Box w="50em">
+              <Flex justifyContent="center" alignItems="center" my="12em">
+                <Box width="360px" height="297px">
+                  <Flex
+                    flexDirection="column"
+                    justifyContent="center"
+                    alignItems="center"
+                    pt={{
+                      ss: "5px",
+                      sm: "5px",
+                      sl: "5px",
+                      md: "19px",
+                      lg: "19px",
                     }}
                     pb="10px"
                   >
@@ -248,7 +232,7 @@ function CompleteFormTenant() {
                           ) : null}
                           {selectedFile ? (
                             <Alert status="info" color="green" text="center">
-                              <i className="fa-solid fa-check"></i>
+                              <i class="fa-solid fa-check"></i>
                               <Text ms="10px">Id Card uploaded</Text>
                             </Alert>
                           ) : null}
